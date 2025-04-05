1ed64d24ed50f6f073483fee4050df232d475db9
Kevin Nielsen <kevinanielsen@outlook.com>
2025-04-05 20:37:07 +0200

    install and implement morgan for http logging

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import { startDB } from './db.js';
 import express from 'express';
 import routes from './routes/index.js';
+import morgan from 'morgan';
+
 const app = express();
 
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
