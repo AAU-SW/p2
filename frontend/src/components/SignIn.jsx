87443eed48c4fdd2205824b12d9785c746e61db4
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-04-07 17:04:38 +0200

    Added input for password

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,16 +1,18 @@
 export const SignIn = () => {
-    return (
-      <>
-        <h1>Sign In</h1>
-        <form>
-          <label>
-            User Email 
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-
-      </>
-    );
-  };
-  
+  return (
+    <>
+      <h1>Sign In</h1>
+      <form style={{ display: 'flex', flexDirection: 'column', gap: '1rem', maxWidth: '300px' }}>
+        <label>
+          User Email:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+};
