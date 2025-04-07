819090c44822f26d13f8edc79d0769ef423965f1
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-04-07 17:00:20 +0200

    Created email input field with form logic.... to be extended

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -2,7 +2,13 @@ export const SignIn = () => {
     return (
       <>
         <h1>Sign In</h1>
-        <button>Click me!</button>
+        <form>
+          <label>
+            User Email 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
 
       </>
     );
