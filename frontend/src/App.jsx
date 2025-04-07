c643b3b5c251baa90697b3ec5d8b69bdf7c31a0c
Isabella Thanh Ly Nguyen <Isabellathanhly@hotmail.com>
2025-04-07 16:36:34 +0200

    Created sign in url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "wouter";
 import "./App.css";
+import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { Home } from "./pages/Home";
 import { Activities } from "./pages/Activities";
@@ -8,6 +9,7 @@ import { Expenses } from "./pages/Expenses";
 import { LogOut } from "./pages/LogOut";
 import { Settings } from "./pages/Settings";
 import { Sidebar } from "./components/Sidebar";
+ 
 
 export const user = {
   userName: "Kevin",
@@ -24,6 +26,9 @@ const App = () => {
         <Switch>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
           </Route>
           <Route path="/sign-up">
             <SignUp />
