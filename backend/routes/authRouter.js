d19ab2a331a2b5ea8319d35acc81894cd1f236f4
Kevin Nielsen <kevinanielsen@outlook.com>
2025-04-14 18:52:43 +0200

    add logout route

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,4 +1,4 @@
-import { Signup, Login } from "../controllers/authController.js";
+import { Signup, Login, Logout } from "../controllers/authController.js";
 import express from "express";
 import { userVerification } from "../middlewares/authMiddleware.js";
 
@@ -7,5 +7,6 @@ const authRouter = express.Router();
 authRouter.post("/", userVerification);
 authRouter.post("/signup", Signup);
 authRouter.post("/login", Login);
+authRouter.get("/logout", Logout);
 
 export default authRouter;
