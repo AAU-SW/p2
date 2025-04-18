8218150515fbc591ec9455141e4e3f56e9391301
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-04-18 16:12:12 +0200

    minor fix

--- frontend/src/components/ExpenseTable.jsx
+++ frontend/src/components/ExpenseTable.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../styles/expenseTable.css";
+import "../styles/ExpenseTable.css";
 import axios from "axios";
 
 export const ExpenseTable = () => {
