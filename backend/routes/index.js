9594db6eb928966c9f71b94234fbb3011c4f5163
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-04-18 16:08:34 +0200

    main fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./authRouter.js";
 import timeplanRoutes from "./timeplanRoutes.js";
+import expenseRouter from "./expenseRouter.js";
 const router = express.Router();
 
 router.use("/auth", authRouter);
