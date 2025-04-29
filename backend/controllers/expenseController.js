466837c163656fe78db2a9e8a9d1f7edcb3fbd7e
Kevin Nielsen <kevinanielsen@outlook.com>
2025-04-29 19:43:40 +0200

    run format

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -3,41 +3,45 @@ import { getUserIdByCookies } from "../util/auth/getUserIdByCookies.js";
 
 // post request to add a new expense
 export const addExpense = async (req, res) => {
-    try {
-        const userId = getUserIdByCookies(req); // Gets current logged in user by using cookie token within browser.
-        if (!userId) {
-            return res.status(401).json({ error: "Unauthorized: Invalid or missing user ID" });
-        }
-        const { expense, amount, date } = req.body;
+	try {
+		const userId = getUserIdByCookies(req); // Gets current logged in user by using cookie token within browser.
+		if (!userId) {
+			return res
+				.status(401)
+				.json({ error: "Unauthorized: Invalid or missing user ID" });
+		}
+		const { expense, amount, date } = req.body;
 
-        if (!expense || !amount || !date) {
-            return res.status(400).json({ error: "All fields are required" });
-        }
+		if (!expense || !amount || !date) {
+			return res.status(400).json({ error: "All fields are required" });
+		}
 
-        const newExpense = new Expense({
-            expense,
-            amount,
-            date,
-            user: userId,
-        }); // Creation of a new expense, with the current logged user.
-        await newExpense.save();
-        res.status(201).json(newExpense);
-    } catch(error){
-        console.error("Error adding expense:", error);
-        res.status(500).json({error: error.message});
-    };
-}
+		const newExpense = new Expense({
+			expense,
+			amount,
+			date,
+			user: userId,
+		}); // Creation of a new expense, with the current logged user.
+		await newExpense.save();
+		res.status(201).json(newExpense);
+	} catch (error) {
+		console.error("Error adding expense:", error);
+		res.status(500).json({ error: error.message });
+	}
+};
 
 // get all expenses for a user
 export const getExpenses = async (req, res) => {
-    try {
-        const userId = getUserIdByCookies(req); // Gets current logged in user by using cookie token within browser.
-        if (!userId) {
-            return res.status(401).json({ error: "Unauthorized: Invalid or missing user ID" });
-        }
-        const expenses = await Expense.find({ user: userId }); // Finds the timeplan of the logged user.
-        res.status(200).json(expenses);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+	try {
+		const userId = getUserIdByCookies(req); // Gets current logged in user by using cookie token within browser.
+		if (!userId) {
+			return res
+				.status(401)
+				.json({ error: "Unauthorized: Invalid or missing user ID" });
+		}
+		const expenses = await Expense.find({ user: userId }); // Finds the timeplan of the logged user.
+		res.status(200).json(expenses);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
