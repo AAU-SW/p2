498d26a398ff614bc2f55e95dcef37df02ceac10
Mathilde Arntkløv <arntklow@gmail.com>
2025-04-30 15:36:18 +0200

    fix

--- backend/routes/timeplanRoutes.js
+++ backend/routes/timeplanRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getTimeplan, postTimeplan } from "../controllers/timeplanController.js";
+import { getTimeplan, postTimeplan, deleteRow } from "../controllers/timeplanController.js";
 const timeplanRouter = express.Router();
 
 // API Routes for posting and fetching timeplans.
 timeplanRouter.post("/", postTimeplan);
 timeplanRouter.get("/", getTimeplan);
+
+timeplanRouter.delete("/", deleteRow);
 
 export default timeplanRouter;
