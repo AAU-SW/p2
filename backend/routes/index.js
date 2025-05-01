6fad13f225390ccc5f17e16f9839c2501dd72387
Kevin Nielsen <kevinanielsen@outlook.com>
2025-05-01 11:38:16 +0200

    add activityRouter to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRouter from "./authRouter.js";
 import timeplanRoutes from "./timeplanRoutes.js";
 import expenseRouter from "./expenseRouter.js";
+import activityRouter from "./activityRouter.js";
 const router = express.Router();
 
 router.get("/", (req, res) => res.send("SpareTime API"));
 router.use("/auth", authRouter);
 router.use("/expenses", expenseRouter);
 router.use("/timeplans", timeplanRoutes);
+router.use("/activities", activityRouter)
 
 export default router;
