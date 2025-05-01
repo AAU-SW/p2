0ecc383adcabb6fe686fb4b41b0391a5dd52c6f0
xamacs <oliver_acs@yahoo.com>
2025-05-01 12:16:05 +0200

    added getActivities

--- backend/controllers/activitiesController.js
+++ backend/controllers/activitiesController.js
@@ -2,21 +2,36 @@ import { Activity } from "../models/activities.js";
 import { getUserIdByCookies } from "../util/auth/getUserIdByCookies.js";
 
 export const addActivity = async (req, res) => {
-	try {
-		const userId = await getUserIdByCookies(req);
-		const { price, title, date } = req.body;
-		const newDate = new Date(date);
-		newDate.setHours(newDate.getHours() + 2);
+  try {
+    const userId = await getUserIdByCookies(req);
+    const { price, title, date } = req.body;
+    const newDate = new Date(date);
+    newDate.setHours(newDate.getHours() + 2);
 
-		const activity = await Activity.create({
-			price,
-			title,
-			date: newDate,
-			user_id: userId,
-		});
-		return res.status(201).json({ activity });
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({ error });
-	}
+    const activity = await Activity.create({
+      price,
+      title,
+      date: newDate,
+      user_id: userId,
+    });
+    return res.status(201).json({ activity });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error });
+  }
+};
+
+export const getActivities = async (req, res) => {
+  try {
+    const userId = getUserIdByCookies(req); // Gets current logged in user by using cookie token within browser.
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const Activities = await Expense.find({ user: userId }); // Finds the timeplan of the logged user.
+    res.status(200).json(Activities);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
