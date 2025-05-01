d8a38d04f4bd4890219ba54173133c6488d1e02c
Oliver Acs <oliver_acs@yahoo.com>
2025-05-01 14:04:24 +0200

    truely postman proof

--- backend/controllers/activitiesController.js
+++ backend/controllers/activitiesController.js
@@ -29,7 +29,7 @@ export const getActivities = async (req, res) => {
         .status(401)
         .json({ error: "Unauthorized: Invalid or missing user ID" });
     }
-    const Activities = await Activity.find({ user: userId });
+    const Activities = await Activity.find({ user_id: userId });
     res.status(200).json(Activities);
   } catch (error) {
     res.status(500).json({ error: error.message });
