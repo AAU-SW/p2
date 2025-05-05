070813b46cda0eee1221d72f643809ceac78ca9e
Mathilde Arntkløv <arntklow@gmail.com>
2025-05-05 21:05:29 +0200

    Activities table

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -1,8 +1,17 @@
+import { ActivitiesTable } from '../components/ActivitiesTable';
+import { useState } from 'react';
+import { Card, CardHeader } from '../components/Card';
+
 export const Activities = () => {
   return (
     <div>
+          <h1>Activities</h1>
+      <Card>
+        <ActivitiesTable />
+      </Card>
 
-    <h1> Activities</h1>
+
     </div>
-  )
+  );
 }
+
