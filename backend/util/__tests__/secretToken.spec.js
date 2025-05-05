b545da08b404885c7b43b0304620a31d178699f0
Kevin Nielsen <kevinanielsen@outlook.com>
2025-05-05 21:25:45 +0200

    run format

--- backend/util/__tests__/secretToken.spec.js
+++ backend/util/__tests__/secretToken.spec.js
@@ -3,17 +3,17 @@ import jwt from "jsonwebtoken";
 import { createSecretToken } from "../secretToken";
 
 describe("secretToken", () => {
-  it("should call jwt.sign", () => {
-    // Arrange
-    const testToken = "test";
-    const signMock = vi.fn().mockReturnValue(testToken);
-    vi.spyOn(jwt, "sign").mockImplementation(signMock);
+	it("should call jwt.sign", () => {
+		// Arrange
+		const testToken = "test";
+		const signMock = vi.fn().mockReturnValue(testToken);
+		vi.spyOn(jwt, "sign").mockImplementation(signMock);
 
-    // Act
-    const result = createSecretToken();
+		// Act
+		const result = createSecretToken();
 
-    // Assert
-    expect(result).toBe(testToken);
-    expect(signMock).toBeCalled();
-  });
+		// Assert
+		expect(result).toBe(testToken);
+		expect(signMock).toBeCalled();
+	});
 });
--- backend/util/auth/__tests__/getUserIdByCookies.spec.js
+++ backend/util/auth/__tests__/getUserIdByCookies.spec.js
@@ -5,34 +5,34 @@ import jwt from "jsonwebtoken";
 const testRequestWithToken = { cookies: { token: "test-token" } };
 
 describe("getUserIdByCookies", () => {
-  it("should throw an error if no token in request", () => {
-    // Arrange
-    const testRequestWithoutToken = { cookies: { token: undefined } };
+	it("should throw an error if no token in request", () => {
+		// Arrange
+		const testRequestWithoutToken = { cookies: { token: undefined } };
 
-    // Act & Assert
-    expect(() => getUserIdByCookies(testRequestWithoutToken)).toThrow();
-  });
-  it("should throw an error if no id in token", () => {
-    // Arrange
-    const testId = undefined;
-    const verifyMock = vi.fn().mockReturnValue({ id: testId });
-    vi.spyOn(jwt, "verify").mockImplementation(verifyMock);
+		// Act & Assert
+		expect(() => getUserIdByCookies(testRequestWithoutToken)).toThrow();
+	});
+	it("should throw an error if no id in token", () => {
+		// Arrange
+		const testId = undefined;
+		const verifyMock = vi.fn().mockReturnValue({ id: testId });
+		vi.spyOn(jwt, "verify").mockImplementation(verifyMock);
 
-    // Act & Assert
-    expect(() => getUserIdByCookies(testRequestWithToken)).toThrow();
-    expect(verifyMock).toBeCalled();
-  })
-  it("should call jwt.verify", () => {
-    // Arrange
-    const testId = "test-id";
-    const verifyMock = vi.fn().mockReturnValue({ id: testId });
-    vi.spyOn(jwt, "verify").mockImplementation(verifyMock);
+		// Act & Assert
+		expect(() => getUserIdByCookies(testRequestWithToken)).toThrow();
+		expect(verifyMock).toBeCalled();
+	});
+	it("should call jwt.verify", () => {
+		// Arrange
+		const testId = "test-id";
+		const verifyMock = vi.fn().mockReturnValue({ id: testId });
+		vi.spyOn(jwt, "verify").mockImplementation(verifyMock);
 
-    // Act
-    const res = getUserIdByCookies(testRequestWithToken);
+		// Act
+		const res = getUserIdByCookies(testRequestWithToken);
 
-    // Assert
-    expect(res).toEqual(testId);
-    expect(verifyMock).toBeCalled();
-  });
+		// Assert
+		expect(res).toEqual(testId);
+		expect(verifyMock).toBeCalled();
+	});
 });
