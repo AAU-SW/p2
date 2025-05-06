4f7b38cf3b34c30e6ab99942ae39187eafeeeba6
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-05-06 22:37:25 +0200

    Activities table (#39)
    * Activities table
    
    * fixed activities
    
    * format
    
    * format
    
    * format
    
    * fix
    
    * format
    
    * total calc fix
    
    * format
    
    ---------
    
    Co-authored-by: Mathilde Arntkløv <arntklow@gmail.com>

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -1,7 +1,13 @@
+import { ActivitiesTable } from '../components/ActivitiesTable';
+import { Card, CardHeader } from '../components/Card';
+
 export const Activities = () => {
   return (
     <div>
-      <h1> Activities</h1>
+      <h1>Activities</h1>
+      <Card>
+        <ActivitiesTable />
+      </Card>
     </div>
   );
 };
