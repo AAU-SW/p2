ee1fc133ba95c0b93754c5694283df54633d2d96
Elias Razi <Kontakt.eliasrazi@gmail.com>
2025-05-13 13:55:43 +0200

    Ran format

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { deleteUser, getUser, updateUser } from "../controllers/userController.js";
+import {
+	deleteUser,
+	getUser,
+	updateUser,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUser);
-userRouter.put("/", updateUser); 
-userRouter.delete("/", deleteUser); 
+userRouter.put("/", updateUser);
+userRouter.delete("/", deleteUser);
 
 export default userRouter;
-
