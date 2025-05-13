9f6fac49bf4b6926affb1e491de9e97efce4c149
Elias <100373290+EliasRazi@users.noreply.github.com>
2025-05-13 14:11:30 +0200

    Create settings page - Change Profile Pictures (Also fixed button comp to square) (#56)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+import { getUserIdByCookies } from "../util/auth/getUserIdByCookies.js";
+import { User } from "../models/users.js";
+
+export const getUser = async (req, res) => {
+	try {
+		const userId = getUserIdByCookies(req);
+		const user = await User.findById(userId);
+		if (!user) return res.status(404).send({ message: "User not found" });
+		res.json(user);
+	} catch (err) {
+		res.status(500).send({ message: "Server error" });
+	}
+};
+
+export const updateUser = async (req, res) => {
+	try {
+		const userId = getUserIdByCookies(req);
+		const { username, email } = req.body;
+		if (!userId) return res.status(401).json({ message: "Unauthorized" });
+		const user = await User.findByIdAndUpdate(userId, { username, email });
+
+		if (!user) return res.status(404).json({ message: "User not found" });
+		res.json(user);
+	} catch (err) {
+		console.error("Error updating user:", err);
+		res.status(500).json({ message: "Server error" });
+	}
+};
+
+export const deleteUser = async (req, res) => {
+	try {
+		const userId = getUserIdByCookies(req);
+		if (!userId) return res.status(401).json({ message: "Unauthorized" });
+		const user = await User.findByIdAndDelete(userId);
+		if (!user) return res.status(404).json({ message: "User not found" });
+		res.json({ message: "User deleted successfully" });
+	} catch (err) {
+		console.error("Error deleting user:", err);
+		res.status(500).json({ message: "Server error" });
+	}
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ import authRouter from "./authRouter.js";
 import timeplanRoutes from "./timeplanRoutes.js";
 import expenseRouter from "./expenseRouter.js";
 import budgetRouter from "./budgetRouter.js";
+import userRouter from "./userRouter.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/auth", authRouter);
 router.use("/expenses", expenseRouter);
 router.use("/timeplans", timeplanRoutes);
 router.use("/budgets", budgetRouter);
+router.use("/user", userRouter);
 
 export default router;
