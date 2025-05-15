9e54b0b1f5d8f047a73cf70d8fb13c98900ec559
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-05-15 13:25:56 +0200

    Feat: Advice page (#62)
    * Feat: Advice page
    
    * format
    
    * added recommendationswidget to overview
    
    * format

---
+++ frontend/src/components/RecommendationsWidget.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { CardHeader, Card, CardDetails } from '../components/Card';
+import { getBudgetsWithCurrentSpending } from '../utils/calculate';
+
+export const RecommendationsWidget = () => {
+  const [overBudgetItems, setOverBudgetItems] = useState([]);
+
+  const DKKFormat = new Intl.NumberFormat('da-DK', {
+    style: 'currency',
+    currency: 'DKK',
+  });
+
+  const fetchBudgetsWithOverSpending = async () => {
+    try {
+      const updatedBudgets = await getBudgetsWithCurrentSpending();
+
+      const overBudget = updatedBudgets.filter(
+        (budget) => budget.currentSpending > budget.maxSpending,
+      );
+      setOverBudgetItems(overBudget);
+    } catch (error) {
+      console.error('Error fetching budgets with spending:', error);
+    }
+  };
+  useEffect(() => {
+    fetchBudgetsWithOverSpending();
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader title="Recommendations" />
+      <CardDetails>
+        {overBudgetItems.length > 0 ? (
+          <div>
+            <h5>Over-Budget Warnings</h5>
+            <ul>
+              {overBudgetItems.map((budget, index) => (
+                <li key={index}>
+                  <strong>{budget.title}</strong>: You have spent{' '}
+                  {DKKFormat.format(budget.currentSpending)} out of{' '}
+                  {DKKFormat.format(budget.maxSpending)}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Great job! You are staying within your budgets.</p>
+        )}
+      </CardDetails>
+    </Card>
+  );
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { MyBudget } from './MyBudget';
 import { useState, useEffect } from 'react';
 import { getBudgetsWithCurrentSpending } from '../utils/calculate';
 import axios from 'axios';
+import { RecommendationsWidget } from '../components/RecommendationsWidget';
 
 export const Home = () => {
   const [budgetSections, setBudgetSections] = useState([]);
@@ -78,62 +79,70 @@ export const Home = () => {
           <MyBudget isWidget={true} />
         </Card>
       </div>
-
-      <div className="budget-widget">
-        <Card style={{ width: '100%' }}>
-          <CardHeader title="Total spending / Budget" />
-          <CardContent>
-            <div className="progress-bar-container">
-              <progress className="progress-bar" value={percentage} max="100" />
-            </div>
-            <div className="budget-amounts">
-              <CardDetails>
-                <div className="percentage-current-amount">
-                  <p>{DKKFormat.format(totalSpent)}</p>
-                  {percentage < 100 ? (
-                    <p className="percentage">{percentage}%</p>
-                  ) : (
-                    <p className="percentage" style={{ color: 'red' }}>
-                      {percentage}%
-                    </p>
-                  )}
+      <section style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ width: '40%' }}>
+          <div className="budget-widget">
+            <Card style={{ width: '100%' }}>
+              <CardHeader title="Total spending / Budget" />
+              <CardContent>
+                <div className="progress-bar-container">
+                  <progress
+                    className="progress-bar"
+                    value={percentage}
+                    max="100"
+                  />
                 </div>
-                <p className="total-amount">{`of ${DKKFormat.format(totalBudget)}`}</p>
-              </CardDetails>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-
-      <div className="budget-widget">
-        <Card style={{ width: '100%' }}>
-          <CardHeader title="Budget / Income" />
-          <CardContent>
-            <div className="progress-bar-container">
-              <progress
-                className="progress-bar"
-                value={Number(percentageIncome)}
-                max="100"
-              />
-            </div>
-            <div className="budget-amounts">
-              <CardDetails>
-                <div className="percentage-current-amount">
-                  <p>{DKKFormat.format(totalBudget)}</p>
-                  {percentageIncome < 100 ? (
-                    <p className="percentage">{percentageIncome}%</p>
-                  ) : (
-                    <p className="percentage" style={{ color: 'red' }}>
-                      {percentageIncome}%
-                    </p>
-                  )}
+                <div className="budget-amounts">
+                  <CardDetails>
+                    <div className="percentage-current-amount">
+                      <p>{DKKFormat.format(totalSpent)}</p>
+                      {percentage < 100 ? (
+                        <p className="percentage">{percentage}%</p>
+                      ) : (
+                        <p className="percentage" style={{ color: 'red' }}>
+                          {percentage}%
+                        </p>
+                      )}
+                    </div>
+                    <p className="total-amount">{`of ${DKKFormat.format(totalBudget)}`}</p>
+                  </CardDetails>
                 </div>
-                <p className="total-amount">{`of ${DKKFormat.format(income)}`}</p>
-              </CardDetails>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
+              </CardContent>
+            </Card>
+          </div>
+
+          <div className="budget-widget">
+            <Card style={{ width: '100%' }}>
+              <CardHeader title="Budget / Income" />
+              <CardContent>
+                <div className="progress-bar-container">
+                  <progress
+                    className="progress-bar"
+                    value={Number(percentageIncome)}
+                    max="100"
+                  />
+                </div>
+                <div className="budget-amounts">
+                  <CardDetails>
+                    <div className="percentage-current-amount">
+                      <p>{DKKFormat.format(totalBudget)}</p>
+                      {percentageIncome < 100 ? (
+                        <p className="percentage">{percentageIncome}%</p>
+                      ) : (
+                        <p className="percentage" style={{ color: 'red' }}>
+                          {percentageIncome}%
+                        </p>
+                      )}
+                    </div>
+                    <p className="total-amount">{`of ${DKKFormat.format(income)}`}</p>
+                  </CardDetails>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <RecommendationsWidget />
+      </section>
     </>
   );
 };
