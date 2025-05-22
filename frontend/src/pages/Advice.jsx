e601e900a74b39a4163d40dddeb5658281e310bf
Kevin Nielsen <kevinanielsen@outlook.com>
2025-05-22 20:57:56 +0200

    fix styling when no overspending

--- frontend/src/pages/Advice.jsx
+++ frontend/src/pages/Advice.jsx
@@ -108,7 +108,7 @@ export const Advice = () => {
         <div
           style={{
             display: 'grid',
-            gridTemplateRows: 'repeat(2, 1fr)',
+            gridTemplateRows: 'repeat(auto-fit, minmax(250px, 1fr))',
             gap: '16px',
           }}
         >
