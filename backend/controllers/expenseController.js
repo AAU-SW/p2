49d64e8ae3b70668f27686aaab37d45abbd15450
Kevin Nielsen <kevinanielsen@outlook.com>
2025-05-23 20:30:07 +0200

    fix month during fetching

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -4,78 +4,82 @@ import { BUDGET_CATEGORIES } from "../util/BUDGET_CATEGORIES.js";
 import { getFirstDateInMonth } from "../util/getFirstDateInMonth.js";
 
 export const addExpense = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req);
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const {
-			expense,
-			amount,
-			date,
-			expenseType = BUDGET_CATEGORIES[0],
-			recurring = false,
-		} = req.body;
+  try {
+    const userId = getUserIdByHeaders(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const {
+      expense,
+      amount,
+      date,
+      expenseType = BUDGET_CATEGORIES[0],
+      recurring = false,
+    } = req.body;
 
-		if (!expense || !amount || !date) {
-			return res.status(400).json({ error: "All fields are required" });
-		}
+    if (!expense || !amount || !date) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
 
-		const newExpense = Expense.create({
-			expense,
-			amount,
-			date,
-			user: userId,
-			expenseType,
-			recurring,
-		});
+    const newExpense = Expense.create({
+      expense,
+      amount,
+      date,
+      user: userId,
+      expenseType,
+      recurring,
+    });
 
-		res.status(201).json(newExpense);
-	} catch (error) {
-		console.error("Error adding expense:", error);
-		res.status(500).json({ error: error.message });
-	}
+    res.status(201).json(newExpense);
+  } catch (error) {
+    console.error("Error adding expense:", error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getExpenses = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req);
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const expenses = await Expense.find({ user: userId });
-		const firstDateInMonth = getFirstDateInMonth();
-		const filteredExpenses = expenses.filter((e) => {
-			if (!e.recurring) {
-				return new Date(e.date) >= firstDateInMonth;
-			}
-			return true;
-		});
-		res.status(200).json(filteredExpenses);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+  try {
+    const userId = getUserIdByHeaders(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const expenses = await Expense.find({ user: userId });
+    const firstDateInMonth = getFirstDateInMonth();
+    const filteredExpenses = expenses.filter((e) => {
+      if (!e.recurring) {
+        const payedDate = new Date(e.date);
+        return (
+          payedDate >= firstDateInMonth &&
+          firstDateInMonth.getFullYear() == payedDate.getFullYear()
+        );
+      }
+      return true;
+    });
+    res.status(200).json(filteredExpenses);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const deleteRow = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req);
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const { id } = req.params;
-		const expense = await Expense.findByIdAndDelete(id);
-		if (!expense) {
-			return res.status(404).json({ error: "Expense not found" });
-		}
-		res.status(200).json({ message: "Expense deleted successfully" });
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+  try {
+    const userId = getUserIdByHeaders(req);
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const { id } = req.params;
+    const expense = await Expense.findByIdAndDelete(id);
+    if (!expense) {
+      return res.status(404).json({ error: "Expense not found" });
+    }
+    res.status(200).json({ message: "Expense deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
--- backend/controllers/timeplanController.js
+++ backend/controllers/timeplanController.js
@@ -3,76 +3,80 @@ import { getUserIdByHeaders } from "../util/auth/getUserIdByHeaders.js";
 import { getFirstDateInMonth } from "../util/getFirstDateInMonth.js";
 
 export const getTimeplan = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const timeplans = await Timeplan.find({ user: userId });
-		const firstDateInMonth = getFirstDateInMonth();
-		const filteredTimeplans = timeplans.filter((t) => {
-			if (t.type === "Variable income") {
-				return new Date(t.workedAt) >= firstDateInMonth;
-			}
-			return true;
-		});
-		res.status(200).json(filteredTimeplans);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+  try {
+    const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const timeplans = await Timeplan.find({ user: userId });
+    const firstDateInMonth = getFirstDateInMonth();
+    const filteredTimeplans = timeplans.filter((t) => {
+      if (t.type === "Variable income") {
+        const workedAtDate = new Date(t.workedAt);
+        return (
+          workedAtDate >= firstDateInMonth &&
+          firstDateInMonth.getFullYear() === workedAtDate.getFullYear()
+        );
+      }
+      return true;
+    });
+    res.status(200).json(filteredTimeplans);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 export const postTimeplan = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const {
-			type,
-			job,
-			date,
-			wage = null,
-			hours = null,
-			jobInterval = null,
-			fixedIncome = null,
-		} = req.body; // Required values to submit a timeplan model, optional fields default to null
+  try {
+    const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const {
+      type,
+      job,
+      date,
+      wage = null,
+      hours = null,
+      jobInterval = null,
+      fixedIncome = null,
+    } = req.body; // Required values to submit a timeplan model, optional fields default to null
 
-		const newTimeplan = await Timeplan.create({
-			type,
-			job,
-			wage,
-			workedAt: date,
-			hours,
-			jobInterval,
-			fixedIncome,
-			user: userId,
-		}); // Creation of a new timeplan, with the current logged user.
-		res.status(201).json(newTimeplan);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+    const newTimeplan = await Timeplan.create({
+      type,
+      job,
+      wage,
+      workedAt: date,
+      hours,
+      jobInterval,
+      fixedIncome,
+      user: userId,
+    }); // Creation of a new timeplan, with the current logged user.
+    res.status(201).json(newTimeplan);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // delete row in timeplan
 export const deleteRow = async (req, res) => {
-	try {
-		const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
-		if (!userId) {
-			return res
-				.status(401)
-				.json({ error: "Unauthorized: Invalid or missing user ID" });
-		}
-		const { id } = req.params; // Gets the id of the row to be deleted.
-		const timeplan = await Timeplan.findByIdAndDelete(id); // Deletes the row with the given id.
-		if (!timeplan) {
-			return res.status(404).json({ error: "Timeplan not found" });
-		}
-		res.status(200).json({ message: "Timeplan deleted successfully" });
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+  try {
+    const userId = getUserIdByHeaders(req); // Gets current logged in user by using cookie token within browser.
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid or missing user ID" });
+    }
+    const { id } = req.params; // Gets the id of the row to be deleted.
+    const timeplan = await Timeplan.findByIdAndDelete(id); // Deletes the row with the given id.
+    if (!timeplan) {
+      return res.status(404).json({ error: "Timeplan not found" });
+    }
+    res.status(200).json({ message: "Timeplan deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
